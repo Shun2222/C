5357470476ad13780ee6b49d0cf84b2140a29432
root <root@DESKTOP-AKBN0LS.localdomain>
2022-06-22 00:10:59 +0900

    add: CMakeLists.txt

---
+++ build/src/CMakeFiles/XCS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XCS.dir/environment.cpp.o"
+  "libXCS.a"
+  "libXCS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XCS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/XCS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for XCS.
+# This may be replaced when dependencies are built.
